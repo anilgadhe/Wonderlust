2f3a11acd53b17d417a91fdb5e33bf9a4ae2a5b6
anilgadhe <gadheanil57@gmail.com>
2025-04-20 13:15:45 +0530

    add home page

--- app.js
+++ app.js
@@ -97,6 +97,10 @@ app.use("/listings",listingRouter)
 app.use("/listings/:id/reviews", reviewRouter)
 app.use(  "/"  ,userRouter)
 
+app.get("/",(req,res)=>{
+   res.render("home");
+})
+
 app.all("*",(req,res,next)=>{
     next(new ExpressError(404 ,"Page Not Found!"));
     
